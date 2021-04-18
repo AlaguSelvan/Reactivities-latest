babd5f4e5e799e4db9d5d3151e301e1cd64a1088
alaguselvan <alaguselvan97@gmail.com>
2021-04-18 22:04:03 +0530

    End of section 9

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -52,6 +52,17 @@ export default class ActivityStore {
     }
   }
 
+  get groupedActivities() {
+    return Object.entries(
+      this.activitiesByDate.reduce((activities, activity) => {
+        console.log(activities, 'activity')
+        const date = activity.date;
+        activities[date] = activities[date] ? [...activities[date], activity] : [activity]
+        return activities;
+      }, {} as {[key: string]: Activity[]})
+    )
+  }
+
   updateActivity = async(activity: Activity) => {
     this.loading = true;
     try {

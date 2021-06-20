845ecc69d51063dca23e600d149e685b5822b3bf
alaguselvan <alaguselvan97@gmail.com>
2021-06-20 20:45:48 +0530

    added postgres

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -7,7 +7,13 @@ import { User, UserFormValues } from '../models/user';
 import { Photo, Profile, UserActivity } from '../models/profile';
 import { PaginatedResult } from '../models/pagination';
 
-axios.defaults.baseURL = 'http://localhost:5000/api';
+axios.defaults.baseURL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+
+const sleep = (delay: number) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, delay)
+  })
+}
 
 axios.interceptors.request.use(config => {
   const token = store.commonStore.token;
@@ -16,7 +22,7 @@ axios.interceptors.request.use(config => {
 })
 
 axios.interceptors.response.use(async response => {
-  await setTimeout(() => {}, 2000)
+  if(process.env.NODE_ENV === "development") await sleep(1000);
   const pagination = response.headers['pagination'];
   if(pagination) {
     response.data = new PaginatedResult(response.data, JSON.parse(pagination));
